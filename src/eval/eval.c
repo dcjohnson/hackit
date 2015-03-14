bd9071587200f52f6d551943abbd483a991b05d7
Dylan Conrad Johnson <dylanthevillain@gmail.com>
2015-03-13 20:23:15 -0500

    Added stack operations for the evalutation of the ast tree.  Havent tested them though.  I should really make my techniques more standard thorught the project.

--- src/eval/eval.c
+++ src/eval/eval.c
@@ -1,5 +1,35 @@
 #import "eval.h"
 
 void eval(ast *tree) {
-	
+	tree_loc_stack *stack = NULL;
+	ast *cur_node = tree;
+	do {
+		stack = push_tree_loc_stack(stack, cur_node);
+	} while (stack != NULL);
+}
+
+tree_loc_stack *push_tree_loc_stack(tree_loc_stack *stack, ast *new_node) {
+	if (stack == NULL) {
+		stack = malloc(sizeof(tree_loc_stack));
+		stack->next = NULL;
+		stack->node.node_ptr = new_node;
+		stack->node.cur_child = 0;
+	} else {
+		tree_loc_stack *new_head = NULL;
+		new_head = malloc(sizeof(tree_loc_stack));
+		new_head->next = stack;
+		new_head->node.node_ptr = new_node;
+		new_head->node.cur_child = 0;
+		stack = new_head;
+	}
+	return stack;
+}
+
+tree_loc_stack *pop_tree_loc_stack(tree_loc_stack *stack) {
+	if (stack != NULL) {
+		tree_loc_stack *new_head = stack->next;
+		free(stack);
+		stack = new_head;
+	}
+	return stack;
 }
--- src/eval/eval.h
+++ src/eval/eval.h
@@ -2,7 +2,24 @@
 #define EVAL_H
 
 #import "../ast/ast.h"
+#import <stdlib.h>
+
+struct tree_loc {
+	ast *node_ptr;
+	int cur_child;
+};
+
+typedef struct tree_loc tree_loc;
+
+struct tree_loc_stack {
+	tree_loc node;
+	struct tree_loc_stack *next;
+};
+
+typedef struct tree_loc_stack tree_loc_stack;
 
 void eval(ast *tree);
+tree_loc_stack *push_tree_loc_stack(tree_loc_stack *stack, ast *new_node);
+tree_loc_stack *pop_tree_loc_stack(tree_loc_stack *stack);
 
 #endif
