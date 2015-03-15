857229c8798f841eb6d4e2abc244321782d943c0
Dylan Conrad Johnson <dylanthevillain@gmail.com>
2015-03-15 16:34:19 -0500

    realization of stupid

--- src/eval/eval.c
+++ src/eval/eval.c
@@ -2,23 +2,22 @@
 
 void eval(ast *tree) {
 	tree_loc_stack *stack = NULL;
-	ast *cur_node = tree;
 	do {
 	} while (stack != NULL);
 }
 
-tree_loc_stack *push_tree_loc_stack(tree_loc_stack *stack, ast *new_node, int child) {
+tree_loc_stack *push_tree_loc_stack(tree_loc_stack *stack, ast *new_node) {
 	if (stack == NULL) {
 		stack = malloc(sizeof(tree_loc_stack));
 		stack->next = NULL;
 		stack->node.node_ptr = new_node;
-		stack->node.cur_child = child;
+		stack->node.cur_child = 0;
 	} else {
 		tree_loc_stack *new_head = NULL;
 		new_head = malloc(sizeof(tree_loc_stack));
 		new_head->next = stack;
 		new_head->node.node_ptr = new_node;
-		new_head->node.cur_child = child;
+		new_head->node.cur_child = 0;
 		stack = new_head;
 	}
 	return stack;
